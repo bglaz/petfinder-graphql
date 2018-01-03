21e42e9fc9b683baf21e7ac7cc68f98866db82d0
Brian Glaz <briang@jkdesign.com>
2018-01-03 11:23:52 -0500

    begin adding Pet graphql type and petGetRandom query

--- src/schema/index.js
+++ src/schema/index.js
@@ -14,13 +14,41 @@ enum AnimalType {
   smallfurry
 }
 
-type Breed {
+enum AnimalSize {
+  S
+  M
+  L
+  XL
+}
+
+enum AnimalStatus {
+  A
+  H
+  P
+  X
+}
+
+enum AnimalGender {
+  M
+  F
+}
+
+type Pet {
+  id: Int!
+  shelterPetId: String
+  age: String!
   name: String!
-  animalType: AnimalType!
+  sex: AnimalGender!
+  size: AnimalSize!
+  status: AnimalStatus!
+  description: String
+  animal: AnimalType!
+  breeds: [String!]
 }
 
 type Query {
-  allBreeds(animal: AnimalType): [Breed!]!
+  breedList(animal: AnimalType!): [String!]!
+  petGetRandom: Pet!
 }
 `;
 
--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -8,19 +8,39 @@ const API_ENDPOINT = 'http://api.petfinder.com';
 const execQuery = function(method, args)
 {
   return axios.get(`${API_ENDPOINT}/${method}`, {
-    params: Object.assign({}, args, {format: 'json', key: API_KEY})
+    params: {...args, format: 'json', key: API_KEY}
     }
   );
 };
 
 module.exports = {
   Query: {
-    async allBreeds(_, args) {
+
+    async breedList(_, args) {
       const method = 'breed.list',
         results = await execQuery(method, args),
         rawBreedList = results.data.petfinder.breeds.breed;
 
-      return rawBreedList.map( function(breed) { return  {name: breed['$t'], animalType: args.animal} });
+      return rawBreedList.map( (breed) => breed['$t'] );
+    },
+
+    async petGetRandom(_, args) {
+      const method = 'pet.getRandom',
+        results = await execQuery(method, {output: 'full', ...args}),
+        rawData = results.data.petfinder.pet;
+
+        return {
+          id: rawData.id['$t'],
+          shelterPetId: rawData.shelterPetId['$t'],
+          age: rawData.age['$t'],
+          name: rawData.name['$t'],
+          sex: rawData.sex['$t'],
+          size: rawData.size['$t'],
+          status: rawData.status['$t'],
+          description: rawData.description['$t'],
+          animal: rawData.animal['$t'].toLowerCase(),
+          breeds: rawData.breeds.breed.length ? rawData.breeds.breed.map( (breed) => breed['$t']) : [rawData.breeds.breed['$t']]
+        }
     }
   }
 };
